01b1eb6ff9ec30cc773503042fca82a56243290e
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-11-04 20:39:10 +0100

    Introduction to Generics

---
+++ src/model/entities/Program.java
@@ -0,0 +1,27 @@
+package model.entities;
+
+import model.services.PrintService;
+
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+//        PrintService<Integer> ps = new PrintService<>();
+        PrintService<String> ps = new PrintService<>();
+
+        System.out.print("How many values? ");
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+//            int value = sc.nextInt();
+            String value = sc.next();
+            ps.addValue(value);
+        }
+
+        ps.print();
+        System.out.println("First: " + ps.first());
+        sc.close();
+    }
+}
