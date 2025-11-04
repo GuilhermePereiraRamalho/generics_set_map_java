82dd134e44d56d3c8d52be15cf463e5d6ad1ee2b
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-11-04 21:03:55 +0100

    Wildcard Types

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,34 +1,17 @@
 package application;
-
-import model.entities.Product;
-import model.services.CalculationService;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
 public class Program {
     public static void main(String[] args) {
-        List<Product> list = new ArrayList<>();
+        List<Integer> myInts = Arrays.asList(5, 2, 10);
+        printList(myInts);
 
-        String path = "C:\\temp\\in.txt";
-
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){
-            String line = br.readLine();
-             while (line != null) {
-                 String[] fields = line.split(",");
-                 list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-                 line = br.readLine();
-             }
-
-            Product x = CalculationService.max(list);
-            System.out.println("Most expensive:");
-            System.out.println(x);
-        }
-        catch (IOException e) {
-            System.out.println("Error: " + e.getMessage());
+        List<String> myStrs = Arrays.asList("Maria", "ALex", "Bob");
+        printList(myStrs);
+    }
+    public static void printList(List<?> list) {
+        for (Object obj : list) {
+            System.out.println(obj);
         }
     }
 }
