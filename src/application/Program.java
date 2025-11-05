88eebcdf64a82b562d8e574fd164d1b44ad19698
Guilherme Ramalho <137944713+GuilhermePereiraRamalho@users.noreply.github.com>
2025-11-05 20:44:23 +0100

    Bounded wildcards - Part 2

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,30 +1,31 @@
 package application;
-import model.entities.Circle;
-import model.entities.Rectangle;
-import model.entities.Shape;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Program {
     public static void main(String[] args) {
-        List<Shape> myShapes = new ArrayList<>();
-        myShapes.add(new Rectangle(3.0, 2.0));
-        myShapes.add(new Circle(2.0));
+        List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+        List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+        List<Object> myObjs = new ArrayList<Object>();
 
-        List<Circle> myCircles = new ArrayList<>();
-        myCircles.add(new Circle(2.0));
-        myCircles.add(new Circle(3.0));
+        copy(myInts, myObjs);
+        printList(myObjs);
+        copy(myDoubles, myObjs);
+        printList(myObjs);
+    }
+    public static void copy(List<? extends Number> source, List<? super Number> destiny){
+        for (Number number : source){
+            destiny.add(number);
+        }
 
-        System.out.println("Total area: " + totalArea(myShapes));
-        System.out.println("Total area: " + totalArea(myCircles));
     }
 
-    public static double totalArea(List<? extends Shape> list){
-        double sum = 0.0;
-        for (Shape s : list){
-            sum += s.area();
+    public static void printList(List<?> list){
+        for (Object obj : list){
+            System.out.print(obj + " ");
         }
-        return sum;
+        System.out.println();
     }
 }
